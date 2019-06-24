a8694882b53749b15f5799abd6f6669b5ee83c7d
Sharansh <sharanshgupta@hotmail.com>
2019-06-24 12:06:37 +0530

    Adding POJOs. Closes issue #2.

--- src/main/java/com/example/springpetclinic/SpringPetClinicApplication.java
+++ src/main/java/com/example/springpetclinic/SpringPetClinicApplication.java
@@ -1,5 +1,7 @@
 package com.example.springpetclinic;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,9 @@ public class SpringPetClinicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringPetClinicApplication.class, args);
+        System.out.println("Sharansh Gupta");
+        Log log = LogFactory.getLog(SpringPetClinicApplication.class);
+        log.info("New log created");
     }
 
 }
